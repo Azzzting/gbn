488985d848151d72cc8899d3f6d3329ee5280826
Azzzting <138744150+Azzzting@users.noreply.github.com>
2024-04-22 15:31:28 +0800

    Update bn254_test_fn_main.go

--- test/bn254_test_fn_main.go
+++ test/bn254_test_fn_main.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
-  "github.com/Azzzting/gbn"
+import(
+	"fmt"
+	"github.com/Azzzting/gbn"
+)
+
+func test_bn(){
+	var a[8] uint32;
+	var b[8] uint32;
+	bn_from_hex(a, 8, hex_a);
+	bn_from_hex(b, 8, hex_b);
+	bn_print(" a = ", a, 8);
+	bn_print(" b = ", b, 8);
+}
+
+func main(){
+	test_bn();
 }
